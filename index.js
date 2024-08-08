8912bda4451b251dceced17bc1933bb1f8df4efd
Augusto <augustovmagro@gmail.com>
2024-08-08 00:06:11 -0300

    Fixing score bug that allow start the game before cliking on 'New Game'

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let gameState = false
+let scoreState = false
 let indexPeriod = 0
 const periods = ["1hf", "2hf", "3hf", "4hf"]
 
 function startGame () {
     if (gameState == false) {
+        indexPeriod = 0
         gameState = true
+        scoreState = true
         text = periods[indexPeriod]
         document.getElementById("game-period").innerText = text
     } else {
@@ -16,6 +19,7 @@ function nextPeriod () {
     indexPeriod += 1
     if (indexPeriod > 3) {
         document.getElementById("game-period").innerText = "Final"
+        scoreState = false
         return
     } else {
         document.getElementById("game-period").innerText = periods[indexPeriod]
@@ -26,10 +30,12 @@ function newGame () {
     document.getElementById("home-score").innerText = "0"
     document.getElementById("guest-score").innerText = "0"
     document.getElementById("game-period").innerText = "Game"
+    gameState = false
+    scoreState = false
 }
 
 function add (score, team) {
-    if (gameState == true) {
+    if (scoreState == true) {
         x = parseInt(document.getElementById(team).innerText)
         x += score
         document.getElementById(team).innerText = x
